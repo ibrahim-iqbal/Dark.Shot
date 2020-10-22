0103de2fc038bbd783b0f1e73f7df40c6ecce88f
Mohd. Ibrahim Iqbal <41406463+ibrahim-iqbal@users.noreply.github.com>
2020-10-22 19:39:25 +0530

    Initial Commit

--- app/src/main/java/io/darkshot/app/view/DashboardActivity.java
+++ app/src/main/java/io/darkshot/app/view/DashboardActivity.java
@@ -1,5 +1,6 @@
 package io.darkshot.app.view;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -9,6 +10,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import io.darkshot.app.R;
 import io.darkshot.app.controller.FeedAdapter;
 
@@ -16,24 +19,55 @@ public class DashboardActivity extends AppCompatActivity {
 
 	FeedAdapter feedAdapter;
 	RecyclerView feed;
+	FloatingActionButton fabDash;
+	String mode = "";
+	SharedPreferences sp;
 
 	@Override
 	protected void onCreate ( Bundle savedInstanceState ) {
 		super.onCreate ( savedInstanceState );
-		setContentView ( R.layout.activity_dashboard );
 
-		Window window = this.getWindow ( );
-		window.clearFlags ( WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS );
-		window.addFlags ( WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS );
-		window.setStatusBarColor ( ContextCompat.getColor ( this , R.color.colorSecondary ) );
-		View decor = getWindow ( ).getDecorView ( );
-		decor.setSystemUiVisibility ( View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR );
+		sp = getSharedPreferences ( "dark_mode" , MODE_PRIVATE );
+		mode = sp.getString ( "mode" , "" );
+
+		if ( mode.equals ( "" ) ) {
+			setTheme ( R.style.Theme_DarkShot_Dark );
+			Window window = this.getWindow ( );
+			window.clearFlags ( WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS );
+			window.addFlags ( WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS );
+			window.setStatusBarColor ( ContextCompat.getColor ( this , R.color.colorSecondary ) );
+		}
+		else {
+			setTheme ( R.style.Theme_DarkShot_Light );
+			Window window = this.getWindow ( );
+			window.clearFlags ( WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS );
+			window.addFlags ( WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS );
+			window.setStatusBarColor ( ContextCompat.getColor ( this , R.color.colorPrimary ) );
+			View decor = getWindow ( ).getDecorView ( );
+			decor.setSystemUiVisibility ( View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR );
+		}
+
+		setContentView ( R.layout.activity_dashboard );
 
 		//ID DECLARATION
 		feed = findViewById ( R.id.feed );
+		fabDash = findViewById ( R.id.fabDash );
 		feedAdapter = new FeedAdapter ( this , 10 );
 		//feed.setDemoLayoutManager ( ShimmerRecyclerView.LayoutMangerType.LINEAR_VERTICAL );
-
 		feed.setAdapter ( feedAdapter );
+
+		fabDash.setOnClickListener ( v -> {
+			sp = getSharedPreferences ( "dark_mode" , MODE_PRIVATE );
+			mode = sp.getString ( "mode" , "" );
+			if ( mode.equals ( "" ) ) {
+				sp.edit ( ).putString ( "mode" , "light" ).apply ( );
+				setTheme ( R.style.Theme_DarkShot_Light );
+			}
+			else {
+				sp.edit ( ).remove ( "mode" ).apply ( );
+				setTheme ( R.style.Theme_DarkShot_Dark );
+			}
+		} );
+
 	}
 }
--- app/src/main/java/io/darkshot/app/view/Splash.java
+++ app/src/main/java/io/darkshot/app/view/Splash.java
@@ -1,27 +1,61 @@
 package io.darkshot.app.view;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.animation.AnticipateInterpolator;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import io.darkshot.app.R;
 
 public class Splash extends AppCompatActivity {
-	
+
 	ImageView sp_logo;
-	
+	String mode;
+
 	@Override
 	protected void onCreate ( Bundle savedInstanceState ) {
 		super.onCreate ( savedInstanceState );
+
+		SharedPreferences sp = getSharedPreferences ( "dark_mode" , MODE_PRIVATE );
+		mode = sp.getString ( "mode" , "" );
+
+		if ( mode.equals ( "" ) ) {
+			setTheme ( R.style.Theme_DarkShot_Dark );
+			Window window = this.getWindow ( );
+			window.clearFlags ( WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS );
+			window.addFlags ( WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS );
+			window.setStatusBarColor ( ContextCompat.getColor ( this , R.color.colorSecondary ) );
+		}
+		else {
+			setTheme ( R.style.Theme_DarkShot_Light );
+			Window window = this.getWindow ( );
+			window.clearFlags ( WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS );
+			window.addFlags ( WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS );
+			window.setStatusBarColor ( ContextCompat.getColor ( this , R.color.colorPrimary ) );
+			View decor = getWindow ( ).getDecorView ( );
+			decor.setSystemUiVisibility ( View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR );
+		}
+
+		Window window = this.getWindow ( );
+		window.clearFlags ( WindowManager.LayoutParams.FLAG_FULLSCREEN );
+		//window.addFlags ( WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS );
+		getWindow ( ).setFlags ( WindowManager.LayoutParams.FLAG_FULLSCREEN , WindowManager.LayoutParams.FLAG_FULLSCREEN );
+		//window.setStatusBarColor ( ContextCompat.getColor ( this , R.color.colorPrimary ) );
+		//View decor = getWindow ( ).getDecorView ( );
+		//decor.setSystemUiVisibility ( View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR );
 		setContentView ( R.layout.activity_main );
-		
+
 		//ID DECLARATION
 		sp_logo = findViewById ( R.id.splogo );
-		sp_logo.animate ( ).scaleX ( 1.2f ).scaleY ( 1.2f ).setInterpolator ( new AnticipateInterpolator ( ) ).setDuration ( 1300 );
+		sp_logo.setOnClickListener ( v -> new Intent ( Splash.this , DashboardActivity.class ) );
+		//sp_logo.animate ( ).scaleX ( 1.2f ).scaleY ( 1.2f ).setInterpolator ( new AnticipateInterpolator ( ) ).setDuration ( 1300 );
 		new Handler ( ).postDelayed ( ( ) -> startActivity ( new Intent ( Splash.this , DashboardActivity.class ) ) , 5000 );
 	}
 }
