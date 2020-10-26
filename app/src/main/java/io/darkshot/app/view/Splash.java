b87fdaede4b234369441c553f7a83709e09bdb0a
ibrahim-iqbal <ibiq1630@gmail.com>
2020-10-26 18:17:53 +0530

    Splash Login designed

--- app/src/main/java/io/darkshot/app/view/Splash.java
+++ app/src/main/java/io/darkshot/app/view/Splash.java
@@ -3,20 +3,20 @@ package io.darkshot.app.view;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
+
+import com.google.android.material.button.MaterialButton;
 
 import io.darkshot.app.R;
 
 public class Splash extends AppCompatActivity {
 
-	ImageView sp_logo;
+	MaterialButton loginbtn;
 	String mode;
 
 	@Override
@@ -45,20 +45,15 @@ public class Splash extends AppCompatActivity {
 
 		Window window = this.getWindow ( );
 		window.clearFlags ( WindowManager.LayoutParams.FLAG_FULLSCREEN );
-		//window.addFlags ( WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS );
 		getWindow ( ).setFlags ( WindowManager.LayoutParams.FLAG_FULLSCREEN , WindowManager.LayoutParams.FLAG_FULLSCREEN );
-		//window.setStatusBarColor ( ContextCompat.getColor ( this , R.color.colorPrimary ) );
-		//View decor = getWindow ( ).getDecorView ( );
-		//decor.setSystemUiVisibility ( View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR );
+
 		setContentView ( R.layout.activity_main );
 
 		//ID DECLARATION
-		sp_logo = findViewById ( R.id.splogo );
-		sp_logo.setOnClickListener ( v -> new Intent ( Splash.this , DashboardActivity.class ) );
-		//sp_logo.animate ( ).scaleX ( 1.2f ).scaleY ( 1.2f ).setInterpolator ( new AnticipateInterpolator ( ) ).setDuration ( 1300 );
-		new Handler ( ).postDelayed ( ( ) -> {
+		loginbtn = findViewById ( R.id.loginbtn );
+		loginbtn.setOnClickListener ( v -> {
 			startActivity ( new Intent ( Splash.this , DashboardActivity.class ) );
 			finish ( );
-		} , 5000 );
+		} );
 	}
 }
